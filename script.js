beba1bffd62d0b528381bff01b1f0f25770da9e1
Paulo henrique <portosilvapaulohenrique0@gmail.com>
2024-07-02 21:05:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ function playRockPaperScissor(player1, player2) {
   ) {
     return "Empate!";
   }
-  player1 === player2;
 
-  console.log(playRockPaperScissor);
   if (
     (player1 === "Pedra" && player2 === "Papel") ||
     (player1 === "Papel" && player2 === "Tesoura") ||
@@ -16,8 +14,6 @@ function playRockPaperScissor(player1, player2) {
   ) {
     return "Jogador 2 venceu!";
   }
-  player1 <= player2;
-  console.log(playRockPaperScissor);
 
   if (
     (player1 === "Pedra" && player2 === "Tesoura") ||
@@ -26,6 +22,6 @@ function playRockPaperScissor(player1, player2) {
   ) {
     return "Jogador 1 venceu!";
   }
-  player1 <= player2;
+
   console.log(playRockPaperScissor);
 }
